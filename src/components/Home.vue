c605a202fb697ec6a5122c559c61dee12ff6df2a
DESKTOP-VT4L1MM\Asus <auiarafat@gmail.com>
2018-09-15 01:27:18 +0600

    finally done

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -6,9 +6,9 @@
       <a class="filters"  @click.prevent="setFilter(2501,5000)" style="cursor:pointer"> £25 to £50 </a> |
       <a class="filters"  @click.prevent="setFilter(5001,10000)" style="cursor:pointer"> £51 to £100 </a> |
       <a class="filters"  @click.prevent="setFilter(10000, 1000000)" style="cursor:pointer"> Over £100 </a> |
-      <span class="filters" @click.stop="clearFilter">Clear</span>
+      <span class="filters" @click.prevent="setFilter(0, 1000000)" style="cursor:pointer">All</span>
     </div>
-    <div class="ui items" v-for="product in products.data" :key="product.id">
+    <div class="ui items" v-for="product in this.products.data" :key="product.id">
       <div class="item">
         <router-link :to="'/products/' + product.slug" class="ui small image">
           <img :src="getProductThumb(product)" alt="">
@@ -35,6 +35,7 @@
     data () {
       return {
         products: {},
+        tempProducts:{},
         prices: ['Under £25', '£25 to £50', '£51 to £100', 'Over £100'],
         filterApplied: [],
         highprice: 300
@@ -73,9 +74,17 @@
         this.filterApplied = []
       },
       setFilter (firstRange, lastRange) { 
+        MoltinService.getHomepageProducts().then((response) => {
+          this.tempProducts = response
+          console.log(this.products + this.products.length)
+        })
+        console.log(Object.keys(this.tempProducts ).length +  " tempPro");
+        if(Object.keys(this.tempProducts ).length!= 0 ){
+          this.products = this.tempProducts
+        }
         var items  = this.products;
 	      var list = [];
-        console.log(items.data)
+        console.log(items)
 				var result = Object.keys(items.data).map(function(key) {
           try{
             console.log(items.data[key].meta + " " + key)
@@ -84,8 +93,8 @@
 
           }
     			});   
-          this.products = list 			
-    			console.log(this.products);
+          this.products.data = list 			
+    			console.log(this.products.data)
       }
     }
   }
